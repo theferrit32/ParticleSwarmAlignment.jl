3dddfa8354555f9e9c3f6f5d49440fb9d54d5999
Kyle Ferriter <kylerferriter@gmail.com>
2019-11-27 16:40:54 -0500

    formatting, and add potential gap improvement in comment

--- needleman_wunsch.jl
+++ needleman_wunsch.jl
@@ -3,11 +3,11 @@
 # TODO for mismatch_penalty, accept a matrix like BLOSUM62 for scoring the
 # mismatch, instead of a static value
 
-function global_align(v,w,match_penalty=1,mismatch_penalty=-1,deletion_penalty=0)
+function global_align(v,w,match_penalty=1,mismatch_penalty=-1,deletion_penalty=-1)
     n1=length(v)
     n2=length(w)
-    s = zeros(Int64,n1+1,n2+1)
-    b = zeros(Int64,n1+1,n2+1)
+    s = zeros(Float64,n1+1,n2+1)
+    b = zeros(Float64,n1+1,n2+1)
 
     for i in 1:(n1+1)
         s[i,1]=(i-1)*deletion_penalty
@@ -20,12 +20,20 @@ function global_align(v,w,match_penalty=1,mismatch_penalty=-1,deletion_penalty=0
 
     for i in 2:(n1+1)
         for j in 2:(n2+1)
-            if v[i-1]==w[j-1]
-                ms=s[i-1,j-1]+match_penalty
+            if v[i-1] == w[j-1]
+                ms = s[i-1,j-1] + match_penalty
             else
-                ms=s[i-1,j-1]+mismatch_penalty
+                # ignore cases where a letter is paired with a gap
+                # do not consider this a mismatch
+                # if v[i-1] != '-' && w[j-1] != '-'
+                #     ms = s[i-1,j-1] + mismatch_penalty
+                # else
+                #     # if a letter is paired with a gap, add no penalty
+                #     ms = s[i-1,j-1] #+ match_penalty # + 0.5 * mismatch_penalty
+                # end
+                ms = s[i-1,j-1] + mismatch_penalty
             end
-            test = [ms,s[i-1,j]+deletion_penalty,s[i,j-1]+deletion_penalty]
+            test = [ms, s[i-1,j] + deletion_penalty, s[i,j-1] + deletion_penalty]
             p = argmax(test)
             s[i,j] = test[p]
             b[i,j] = p
